b650f56a47e6427f4a5e455dd11034556132dcad
Shubhajit07 <shubhajith85@gmail.com>
2023-03-03 14:55:51 +0530

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash                                                       
 # Define colors                                                   
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 # Check if required packages are already installed
